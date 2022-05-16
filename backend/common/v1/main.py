1bead813a8d13d6c7273a6b1719335d8d029d8e6
Ardimas Purwita <ardimasandipurwita@outlook.com>
2022-05-16 15:38:33 +0700

    add pointer system and list based on the pointer

--- backend/common/v1/main.py
+++ backend/common/v1/main.py
@@ -19,6 +19,10 @@ import random
 import shutil
 import threading
 
+import pymongo
+from bson.objectid import ObjectId
+mongo_client = pymongo.MongoClient("mongodb://mongo:27017/")
+
 _uid = threading.local()
 
 os.environ['TZ'] = 'Asia/Jakarta'
@@ -82,6 +86,7 @@ async def upload_files(
     userid: str,
     files: List[UploadFile] = File(..., description="Upload multiple files"),
     foldername: str = Form(..., description="The folder name (e.g., use '.' to store the files in the root folder)"),
+    pointer: str = Form(..., description="Pointer name (e.g., showcase). Please use '*' for the general use."),
 ):
     try:
         
@@ -101,6 +106,13 @@ async def upload_files(
                 async with aiofiles.open(filename, 'wb') as out_file:
                     content = await file.read() 
                     await out_file.write(content) 
+
+                # save to db
+                if pointer != '*':
+                    to_db = {"path":filename}
+                    user_db = mongo_client[urllib.parse.quote(f"{userid}")]
+                    list_pointers = user_db[urllib.parse.quote(f"{pointer}")]
+                    list_pointers.insert_one(to_db).inserted_id
 
                 results.append(True)
             except:
@@ -174,6 +186,39 @@ def get_list_folders_and_files(userid:str):
         return {
             "status":"success",
             "data": get_tree("files/"+urllib.parse.quote(f"{userid}"),userid)
+            }
+    except Exception as e:
+        return {"status":"failed", "message": str(e)}
+
+def get_tree_filtered(path,userid,pointer,list_paths):
+    tmp = [
+       {
+           'id': genuid()[-1],
+        #    'name':os.path.join(path,k).replace("files/"+urllib.parse.quote(f"{userid}"),''),
+           'name':os.path.join(path,k).split('/')[-1],
+           'type': 'file' if os.path.isfile(os.path.join(path,k)) else 'folder',
+           'uploaddate': time.ctime(os.path.getctime(os.path.join(path, k))) if os.path.isfile(os.path.join(path,k)) else None,
+           'filetype' :mime.from_file(os.path.join(path, k)) if os.path.isfile(os.path.join(path,k)) else None,
+           'children' : get_tree_filtered(os.path.join(path,k),userid,pointer,list_paths) if os.path.isdir(os.path.join(path,k)) else None
+       } for k in os.listdir(path) if any([k in xx for xx  in list_paths])
+    ]
+    result = [cleanNullTerms(t) for t in tmp]
+    return result
+
+@app.get("/files/{userid}/lists/{pointer}", tags=["files"])
+def get_list_folders_and_files_based_on_pointer(userid:str,pointer:str):
+    try:
+        user_db = mongo_client[urllib.parse.quote(f"{userid}")]
+        list_pointers = user_db[urllib.parse.quote(f"{pointer}")]
+        list_paths = [object["path"] for object in list_pointers.find()]
+        result = get_tree_filtered(
+            "files/"+urllib.parse.quote(f"{userid}"),
+            userid,
+            pointer,
+            list_paths)
+        return {
+            "status":"success",
+            "data": result
             }
     except Exception as e:
         return {"status":"failed", "message": str(e)}
