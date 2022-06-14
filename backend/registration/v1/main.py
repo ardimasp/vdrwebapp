3ae84b5a2fe4b46e2795f4c65116f3de028e608f
Ubuntu <ubuntu@ip-172-31-22-12.ap-southeast-1.compute.internal>
2022-06-14 07:25:51 +0000

    fix bug in removing template.xlsx

--- backend/registration/v1/main.py
+++ backend/registration/v1/main.py
@@ -418,8 +418,11 @@ async def edit_profile(profile: EditProfile, current_user: User = Depends(get_cu
                 filename = "files/"+(f"{profile.userid}")
                 shutil.copyfile("/app/app/template.xlsx", filename+"/template.xlsx")
             elif profile.type == UserType.regular:
-                filename = "files/"+(f"{profile.userid}")
-                os.remove(filename+"/template.xlsx")
+                try:
+                    filename = "files/"+(f"{profile.userid}")
+                    os.remove(filename+"/template.xlsx")
+                except:
+                    pass
 
             return {"status": "success"}
         else:
